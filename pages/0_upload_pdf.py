e844a321f573b8153cda6648224e580769034979
oveedh <161175477+oveedh@users.noreply.github.com>
2024-03-10 19:08:37 +0000

    added gitignore

--- pages/0_upload_pdf.py
+++ pages/0_upload_pdf.py
@@ -25,7 +25,7 @@ import os
 import streamlit as st
 from streamlit.logger import get_logger
 import PyPDF2
-import pinecone
+from pinecone import Pinecone
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import PyPDFLoader
 import hashlib
@@ -37,7 +37,9 @@ PINECONE_API_KEY=os.environ['PINECONE_API_KEY']
 PINECONE_API_ENV=os.environ['PINECONE_API_ENV']
 PINECONE_INDEX_NAME=os.environ['PINECONE_INDEX_NAME']
 
-client=OpenAI(api_key=os.environ['OPENAI_API_KEY'])
+new_key = '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'
+#client=OpenAI(api_key=os.environ['OPENAI_API_KEY'])
+client=OpenAI(api_key=new_key)
 
 def pdf_to_text(uploaded_file):
     pdfReader = PyPDF2.PdfReader(uploaded_file)
@@ -49,8 +51,8 @@ def pdf_to_text(uploaded_file):
     return text
 
 def embed(text,filename):
-    pinecone.init(api_key=PINECONE_API_KEY, environment=PINECONE_API_ENV)
-    index = pinecone.Index(PINECONE_INDEX_NAME)
+    pc = Pinecone(api_key=PINECONE_API_KEY)
+    index = pc.Index(PINECONE_INDEX_NAME)
     text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000,chunk_overlap  = 200,length_function = len,is_separator_regex = False)
     docs=text_splitter.create_documents([text])
     for idx,d in enumerate(docs):
